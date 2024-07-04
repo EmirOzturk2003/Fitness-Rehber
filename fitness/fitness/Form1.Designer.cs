46060fed35e6a55f9c4b11bba40cc909d60464dc
Emir Ozturk <emir.ozturk.gm@outlook.com>
2024-07-04 17:48:01 +0300

    Fitness Rehberi
    Yeni başlayanlar için oldukça kullanışlı bir  uygulamadır. Uygulamada bir web sitesinden kaynak alındığı için gerekli dosyaları kurmanız gerek

---
+++ fitness/fitness/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace fitness
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gogus1 = new System.Windows.Forms.Button();
+            this.webView1 = new EO.WebBrowser.WebView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.webControl1 = new EO.WinForm.WebControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(340, 139);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1282, 891);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // gogus1
+            // 
+            this.gogus1.BackColor = System.Drawing.Color.Transparent;
+            this.gogus1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gogus1.Location = new System.Drawing.Point(674, 348);
+            this.gogus1.Name = "gogus1";
+            this.gogus1.Size = new System.Drawing.Size(37, 24);
+            this.gogus1.TabIndex = 1;
+            this.gogus1.Text = "look";
+            this.gogus1.UseVisualStyleBackColor = false;
+            this.gogus1.Click += new System.EventHandler(this.gogus1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(1708, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "çık";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(661, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(38, 24);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "look";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(714, 452);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(37, 24);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "look";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(651, 428);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 24);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "look";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(613, 328);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(37, 24);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "look";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(587, 395);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(37, 24);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "look";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(530, 482);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(37, 24);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "look";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(651, 661);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(37, 24);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "look";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(613, 827);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(44, 24);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "look";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Transparent;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Location = new System.Drawing.Point(1411, 482);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(37, 24);
+            this.button10.TabIndex = 13;
+            this.button10.Text = "look";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.Transparent;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Location = new System.Drawing.Point(1224, 452);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(37, 24);
+            this.button11.TabIndex = 14;
+            this.button11.Text = "look";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.Transparent;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Location = new System.Drawing.Point(1357, 395);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(37, 24);
+            this.button12.TabIndex = 15;
+            this.button12.Text = "look";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.Transparent;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button13.Location = new System.Drawing.Point(1224, 348);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(37, 24);
+            this.button13.TabIndex = 16;
+            this.button13.Text = "look";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.Transparent;
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button14.Location = new System.Drawing.Point(1224, 296);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(37, 24);
+            this.button14.TabIndex = 17;
+            this.button14.Text = "look";
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.Transparent;
+            this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button15.Location = new System.Drawing.Point(1316, 318);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(37, 24);
+            this.button15.TabIndex = 18;
+            this.button15.Text = "look";
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.Color.Transparent;
+            this.button16.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button16.Location = new System.Drawing.Point(1277, 395);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(37, 24);
+            this.button16.TabIndex = 19;
+            this.button16.Text = "look";
+            this.button16.UseVisualStyleBackColor = false;
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.Color.Transparent;
+            this.button17.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button17.Location = new System.Drawing.Point(1253, 558);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(37, 24);
+            this.button17.TabIndex = 20;
+            this.button17.Text = "look";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.Color.Transparent;
+            this.button18.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button18.Location = new System.Drawing.Point(1277, 661);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(37, 24);
+            this.button18.TabIndex = 21;
+            this.button18.Text = "look";
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.Color.Transparent;
+            this.button19.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button19.Location = new System.Drawing.Point(1299, 827);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(37, 24);
+            this.button19.TabIndex = 22;
+            this.button19.Text = "look";
+            this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // webControl1
+            // 
+            this.webControl1.BackColor = System.Drawing.Color.White;
+            this.webControl1.Location = new System.Drawing.Point(-5, 139);
+            this.webControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.webControl1.Name = "webControl1";
+            this.webControl1.Size = new System.Drawing.Size(1938, 1080);
+            this.webControl1.TabIndex = 23;
+            this.webControl1.Text = "webControl1";
+            this.webControl1.WebView = this.webView1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.webControl1);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gogus1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button gogus1;
+        private EO.WebBrowser.WebView webView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private EO.WinForm.WebControl webControl1;
+    }
+}
+
---
+++ fitness/fitness/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fitness
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webControl1.Hide();
+        }
+
+        private void webControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+        void go(string adres)
+        {
+            webView1.Url = adres.ToString();
+            webControl1.Show();
+        }
+        private void gogus1_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/chest");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webView1.Url = "www.google.com";
+            webControl1.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/traps");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/shoulders");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/biceps");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/forearms");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/obliques");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/abdominals");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/quads");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/calves");
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/traps");
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/shoulders");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/triceps");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/forearms");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/traps_middle");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/lowerback");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/hamstrings");
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/calves");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            go("https://musclewiki.com/exercises/male/glutes");
+        }
+    }
+}
